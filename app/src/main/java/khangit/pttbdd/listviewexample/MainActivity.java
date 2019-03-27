52bf98b6064cf6deb1fd1b5897d546e9dfd0b91d
khangit <vuthuakhangit@gmail.com>
2019-03-27 14:54:51 +0700

    done example extends listactivity (worked)

--- app/src/main/java/khangit/pttbdd/listviewexample/MainActivity.java
+++ app/src/main/java/khangit/pttbdd/listviewexample/MainActivity.java
@@ -1,31 +1,34 @@
 package khangit.pttbdd.listviewexample;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
     TextView txtMsg;
+    ListView myListView;
 
     String[] items = {"D0","D01","D012","D013","D014","D015","D016","D017","D018","D019","D0110"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setListAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items));
+        myListView = findViewById(R.id.my_list);
+        ArrayAdapter<String> aa = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
+        myListView.setAdapter(aa);
         txtMsg = findViewById(R.id.txtMsg);
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        String text = " Position: " + position + " " + items[position];
-        txtMsg.setText(text);
-    }
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String text = "Position: " + position + "\nData: " + items[position];
+                txtMsg.setText(text);
+            }
+        });
+    }//onCreate
 
 
 }
